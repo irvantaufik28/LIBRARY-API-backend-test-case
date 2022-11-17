f9a1b6405f29e782df91fee65b9e53d7cce5769a
irvan taufik <irvantaufik28@gmail.com>
2022-11-17 18:24:00 +0700

    add controller member

---
+++ src/controllers/member.js
@@ -0,0 +1,52 @@
+const resData = require('../helper/response');
+
+module.exports = {
+  getAllMember: async (req, res, next) => {
+    try {
+      const result = await req.memberUC.getAllMember();
+
+      return res.status(result.statusCode).json(resData.success(result.data));
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  getMemberById: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const result = await req.getMemberById(id);
+
+      if (!result.isSuccess) {
+        return res
+          .status(result.statusCode)
+          .json(resData.failed(result.reason));
+      }
+
+      return res.status(result.statusCode).json(resData.success(result.data));
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  addMember: async (req, res, next) => {
+    try {
+      const member = {
+        name: req.body.name,
+        email: req.body.email,
+      };
+
+      const result = await req.addMember(member);
+
+      if (!result.isSuccess) {
+        return res
+          .status(result.statusCode)
+          .json(resData.failed(result.reason));
+      }
+
+      return res.status(result.statusCode).json(resData.success(result.data));
+    } catch (error) {
+      next(error);
+    }
+  },
+};
